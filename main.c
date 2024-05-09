9f0ee41ff867517843f240c56926af4a43ba5d0e
jermany17 <leejm5050@naver.com>
2024-05-09 22:58:41 +0900

    프로젝트 생성

---
+++ main.c
@@ -0,0 +1,12 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <Windows.h>
+#include <time.h>
+#include <conio.h>
+
+int main() {
+
+    return 0;
+}
