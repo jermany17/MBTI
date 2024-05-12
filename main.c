c2b6ff881b5177430a557c1b0a67b5a88fde5019
김시현 <115911990+SihyunEllen@users.noreply.github.com>
2024-05-12 20:42:34 +0900

    질문출력
    질문출력만  넣어줌, 직업추천은 나중에 할께

--- main.c
+++ main.c
@@ -7,11 +7,31 @@
 #include <conio.h>
 #include "sia_compTable.h"
 
+#define QUESTION_NUM 12 // 질문의 총 개수
+int sih_used[QUESTION_NUM] = { 0 };
+char sih_Q[QUESTION_NUM][100] = {
+
+    "당신은 친구들과 약속잡는 것을 좋아하나요? (Y/N)",
+    "혼자있을 때 외로움을 많이 타나요? (Y/N)",
+    "사람들이 많은 곳에 자주 가나요? (Y/N)" //0~2번은 E/I인 경우(Y일 때 E임)
+    "상상에 빠져들기 쉬우신가요? (Y/N)",
+    "새로운 경험을 추구하는 편인가요? (Y/N)",
+    "4차원 같다는 말을 많이 들으시나요? (Y/N)", // 3~5번은 N/S인 경우(Y일 때 N임)
+    "타인의 감정에 관심없는 편인가요? (Y/N)",
+    "공감해줘라는 말을 많이 듣는 편인가요 (Y/N)",
+    "감정보다 이성을 따르는 편인가요? (Y/N)", // 6~8번은 F/T (Y일 때 T임)
+    "규칙적인 일상을 선호하시나요? (Y/N)",
+    "마감기한을 잘 지키시나요? (Y/N)",
+    "휴식을 취하기 전에 집안일을 먼저 끝내시나요? (Y/N) " // 9~11번은 J/P (Y일 때 J임)
+
+};
 void sia_compCheck();
+int sih_shuffleQ();
+void sih_printQ();
 
 int main() {
     sia_compCheck();
-
+    sih_printQ();
     return 0;
 }
 
@@ -19,9 +39,9 @@ void sia_compCheck() {
     char type1[5], type2[5];
     int i = 0, j = 0;
 
-    printf("ù ��° MBTI ������ �Է��ϼ���: ");
+    printf("첫 번째 MBTI 유형을 입력하세요: ");
     scanf("%s", type1);
-    printf("�� ��° MBTI ������ �Է��ϼ���: ");
+    printf("두 번째 MBTI 유형을 입력하세요: ");
     scanf("%s", type2);
 
     for (int i = 0; i < 4; i++) { // standardisation of input
@@ -29,7 +49,7 @@ void sia_compCheck() {
         if (type2[i] >= 'a' || type2[i] <= 'z') type2[i] -= 32;
     }
 
-    // MBTI ������ ���� �ε����� ��ȯ
+    // MBTI 유형을 숫자 인덱스로 변환
     if (type1[0] == 'E') i += 8;
     if (type2[0] == 'E') j += 8;
 
@@ -42,8 +62,30 @@ void sia_compCheck() {
     if (type1[3] == 'J') i += 1;
     if (type2[3] == 'J') j += 1;
 
-    // ���� ���� ���
+    // 궁합 점수 계산
     int compatibility_score = sia_compatibility_table[i][j];
 
-    printf("%s�� %s�� ���� ������ %d%%�Դϴ�.\n", type1, type2, compatibility_score);
+    printf("%s와 %s의 궁합 점수는 %d%%입니다.\n", type1, type2, compatibility_score);
+}
+ 
+int sih_shuffleQ()
+{
+    int idx;
+    do {
+        idx = rand() % QUESTION_NUM;
+    } while (sih_used[idx]); 
+    sih_used[idx] = 1; 
+    return idx;
+}
+
+void sih_printQ()
+{
+    char answer;
+    for (int i = 0; i < QUESTION_NUM; i++)
+    {
+        int idx = sih_shuffleQ();
+        printf("%d. %s\n", i + 1, sih_Q[idx]);
+        scanf(" %c", &answer);
+        /*여기서 Y/N입력 받은 거 잘 카운트 하셈*/
+    }
 }
